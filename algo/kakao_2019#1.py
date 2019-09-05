c956f537916b9b8417d3613fd9ff3f38ae3753e8
azmoadys <azmoadys@gmail.com>
2019-09-06 00:24:57 +0900

    update

--- algo/kakao_2019#1.py
+++ algo/kakao_2019#1.py
@@ -1,6 +1,7 @@
 # https://tech.kakao.com/2018/09/21/kakao-blind-recruitment-for2019-round-1/
 
 record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+import sys
 
 def solution(record):
     answer = []
@@ -9,16 +10,20 @@ def solution(record):
     for words in record:
         word = words.split() # [0]==command / [1]==UID / [2]==Nickname
         if word[0] == 'Enter':
-            dic.update({word[1]:[word[0],word[2],number]})
+            dic.update({word[1]:word[2]}) # UID:Nickname
         elif word[0] == 'Change':
             temp = dic[word[1]]
-            dic.update({word[1]:[temp[0],word[2],temp[2]]})
-        elif word[0] =='Leave':
-            answer.append(1)
+            dic.update({word[1]:word[2]})
+        #elif word[0] =='Leave':
+            #answer.append(1)
+    for words in record:
+        word = words.split()
+        if word[0] =='Enter':
+            answer.append(dic[word[1]]+"님이 들어왔습니다.")
+        if word[0] == 'Leave':
+            answer.append(dic[word[1]]+"님이 나갔습니다.")
             
-        number += 1
-            
-    print(dic)
+#    print(dic)
     print(answer)
     return answer
 
