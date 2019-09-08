1c66d8ec9fe925cbe7b62195b4212e336178ffff
azmoadys <azmoadys@gmail.com>
2019-09-09 05:27:10 +0900

    update

---
+++ algo/bj2609.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/2609
+
+import sys
+
+def gcd(num1, num2):
+    temp = num1 - num2
+    if num2 == 0:
+        result =  num1
+    else :
+        result = gcd(max(num2, temp), min(num2, temp))
+    return result
+
+def lcm(n1, n2, g_n):
+    return int(n2*(n1/g_n))
+
+def solution():
+    n1, n2 = map(int, sys.stdin.readline().split())
+    g_n = gcd(max(n1, n2), min(n1, n2))
+    print(g_n)
+    print(lcm(n1, n2, g_n))
+
+if __name__ == '__main__':
+    solution()
--- algo/ks_2019E_cherriesMesh.py
+++ algo/ks_2019E_cherriesMesh.py
@@ -1,15 +1,32 @@
 # problem URL
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edb/0000000000170721
-# 4th Sep
+
+# algorithm == path finding
 
 def selection():
-    n,m = map(int, sys.stdin.readline().split())
-    result = n+m
-    return str(result)
+    # black == 1 sugar,red == 2 sugar
+    n,m = map(int, sys.stdin.readline().split()) # n = # of cherries / m = # of black strand
+    #b_c = sys.stdin.readline().split()
+    b_c = []
+    while True:
+        try:
+            c,d = map(int,sys.stdin.readline().split())
+            b_c.append([c,d])
+        except:
+            break
+    #for i in range(len(b_c)):
+    #    b_c[i] = int(b_c[i])
+
+    for i in range(1,n+1):
+        for j in range(i+1, n+1):
+            if [i,j]        
+ 
+    print(b_c)
+    return result
 
 import sys
 
 test_case = int(input())
 
 for i in range(test_case):
-     sys.stdout.write(selection())
+     print('Case #',i,': ',selection())
