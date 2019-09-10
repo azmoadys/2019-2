617491a8371194bcc44c9b6c5fe2cf190f3faac7
azmoadys <azmoadys@gmail.com>
2019-09-10 14:34:46 +0900

    O(nlog(n)) solution

--- algo/mp01sep.py
+++ algo/mp01sep.py
@@ -2,13 +2,22 @@ integer = [8, 7, 2, 5, 3, 1]
 #S = 10
 
 def solution(integer, S):
-    for i in integer:
-        temp = integer
-        temp.remove(i)
-        if S-i in temp:
-            print('(',i,'+',S-i,'=',S,')')
+    integer.sort()
+    low = 0
+    high = len(integer)-1
+    if integer[high-1] +integer[high] < S:
+        return False
+    while low < high :
+        if integer[low] +integer[high] == S:
+            print(integer[low], integer[high])
             return True
+        elif integer[low] +integer[high] > S:
+            high -=1
+        else:
+            low+=1
     return False
+
+
 
 if __name__ == '__main__':
     S = int(input())
