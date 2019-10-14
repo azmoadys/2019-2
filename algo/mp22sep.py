e112a00783b74ddbde601faef723d1eb11c4edf8
azmoadys <azmoadys@gmail.com>
2019-10-14 20:30:24 +0900

    O(N) solution

---
+++ algo/mp22sep.py
@@ -0,0 +1,16 @@
+Input= [0, 1, 2, 2, 1, 0, 0, 2, 0, 1, 1, 0]
+Output = []
+
+for i in Input:
+    if i ==0:
+        Output.append(i)
+
+for i in Input:
+    if i ==1:
+        Output.append(i)
+
+for i in Input:
+    if i ==2:
+        Output.append(i)
+
+print(Output)
